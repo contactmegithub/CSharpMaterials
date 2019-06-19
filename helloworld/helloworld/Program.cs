0eeec8f26d6a116d270fd4a3e051deab92d2d5c2
contactmegithub <yasin9944@gmail.com>
2019-06-19 23:29:45 +0530

    FirstProgram

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! welcome to CSharp with HopeTutors");
+            Console.ReadLine();
+        }
+    }
+}
