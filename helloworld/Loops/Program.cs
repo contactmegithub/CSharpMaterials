fc41e4d12d673c43ca685a89239b59f73e2c05e6
contactmegithub <yasin9944@gmail.com>
2019-07-05 10:52:09 +0530

    updates

---
+++ helloworld/Loops/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    public class Program
+    {
+
+        #region While samples
+        public static void WSample1()
+        {
+            /* local variable definition */
+            int a = 10;
+
+            /* while loop execution */
+            while (a < 20)
+            {
+                Console.WriteLine("value of a: {0}", a);
+                a++;
+            }
+            Console.ReadLine();
+        }
+        #endregion
+
+        #region for Samples
+        public static void forSample1()
+        {
+            /* for loop execution */
+            for (int a = 10; a < 20; a = a + 1)
+            {
+                Console.WriteLine("value of a: {0}", a);
+            }
+            Console.ReadLine();
+        }
+        #endregion
+
+        #region do while samples
+        public static void DowhileSample1()
+        {
+            /* local variable definition */
+            int a = 10;
+
+            /* do loop execution */
+            do
+            {
+                Console.WriteLine("value of a: {0}", a);
+                a = a + 1;
+            }
+            while (a < 20);
+            Console.ReadLine();
+        }
+        #endregion
+
+        #region nested loops
+        public static void NestedLoopSample1()
+        {
+            /* local variable definition */
+            int i, j;
+
+            for (i = 2; i < 100; i++)
+            {
+                for (j = 2; j <= (i / j); j++)
+                    if ((i % j) == 0) break; // if factor found, not prime
+                if (j > (i / j)) Console.WriteLine("{0} is prime", i);
+            }
+            Console.ReadLine();
+        }
+        #endregion
+
+        #region Loop Control Statements
+        public static void BreakStatmet()
+        {
+            /* local variable definition */
+            int a = 10;
+
+            /* while loop execution */
+            while (a < 20)
+            {
+                Console.WriteLine("value of a: {0}", a);
+                a++;
+
+                if (a > 15)
+                {
+                    /* terminate the loop using break statement */
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+
+        public static void ContinueStatmet()
+        {
+            /* local variable definition */
+            int a = 10;
+
+            /* do loop execution */
+            do
+            {
+                if (a == 15)
+                {
+                    /* skip the iteration */
+                    a = a + 1;
+                    continue;
+                }
+                Console.WriteLine("value of a: {0}", a);
+                a++;
+            }
+            while (a < 20);
+            Console.ReadLine();
+        }
+        #endregion
+        public static void LoopSamples()
+        {
+            ContinueStatmet();
+        }
+        public static void Main(string[] args)
+        {
+            LoopSamples();
+        }
+    }
+}
---
+++ helloworld/Methods/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class NumberManipulator
+    {
+        public int FindMax(int num1, int num2)
+        {
+            /* local variable declaration */
+            int result;
+
+            if (num1 > num2)
+                result = num1;
+            else
+                result = num2;
+            return result;
+        }
+    }
+    class Program
+    {
+        public int FindMax(int num1, int num2)
+        {
+            /* local variable declaration */
+            int result;
+
+            if (num1 > num2)
+                result = num1;
+            else
+                result = num2;
+
+            return result;
+        }
+        #region Recursive Method Call
+        /// <summary>
+        /// A method can call itself. This is known as recursion. Following is an example that calculates factorial for a given number using a recursive function −
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public int factorial(int num)
+        {
+            /* local variable declaration */
+            int result;
+            if (num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                result = factorial(num - 1) * num;
+                return result;
+            }
+        }
+        #endregion
+
+        #region Passing Parameters by Value
+
+        #endregion
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
